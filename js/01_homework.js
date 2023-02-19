a2c1ab298f6b9207297a5359e851ab5fb662c89b
Denys Voinash <desdenisandqwerty@gmail.com>
2023-02-19 13:02:42 +0200

    add hw

---
+++ js/01_homework.js
@@ -0,0 +1,41 @@
+// ЗАДАЧА 5
+// Напишіть ф-цію calculateAverage() яка приймає довільну кількість аргументів і повертає їх середнє значення .Всі аргументи будуть тільки числовими
+
+// // console.log(calculateAverage(1, 2, 3, 4)); // 2.5
+// // console.log(calculateAverage(14, 8, 2)); // 8
+// // console.log(calculateAverage(27, 43, 2, 8, 36));// 23.2
+
+// ЗАДАЧА 6
+// Напишіть ф-цію greet(name) , яка при виклику буде получати імя (як приклад , "Василь"), і логінувати рядок "Привіт, <імя>".У випадку , відсутнього
+// відсутнього значення, виводе аргумент "Привіт , гість"
+// // console.log(greet("Василь"));//Привіт Василь
+// // console.log(greet());//Привіт гість
+
+// ЗАДАЧА 7
+// Виконайте рефакторинг замінивши обявлення ф-ції з function declaration на function expression та на стрілочну функцію
+// // function checkNumbers(a, b) {
+// //   if (a > b) {
+// //     return `число ${a} більше ${b}`;
+// //   }
+
+// //   return `число ${b} більше ${a}`;
+// // }
+
+// ЗАДАЧА 8
+// Напишіть ф-цію capitalize , яка буде приймати рядок і буде повертати новий рядок , де кожне слово буде з великої букви
+// // capitalize('the quick brown fox')// 'The Quick Brown Fox '
+
+// ЗАДАЧА 9
+// Напишіть функцію для роботи з колекцією навчаючих курсів courses:
+
+// addCourse(name) - добавляє курс у кінець колекції
+// removeCourse(name) - видаляє курс із колекції
+// updateCourse(oldName, newName)- змінює імя на нове
+// const courses = ["HTML", "CSS", "JavaScript", "React", "PostgreSQL"];
+
+// // addCourse("Express"); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
+// // addCourse("CSS"); // 'У вас уже є такий курс'
+// // removeCourse("React"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'Express']
+// // removeCourse("Vue"); // 'Курс з таким іменем не найдено'
+// // updateCourse("Express", "NestJS"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
+// console.log(courses);
