7c593c776dbbf3740a3e2744051b30a8a4bd613f
Denys Voinash <desdenisandqwerty@gmail.com>
2023-02-19 11:13:17 +0200

    1-3 task

---
+++ js/lesson_1.js
@@ -0,0 +1,26 @@
+// ЗАДАЧА 1
+// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+
+// const userName = "ГАЛИНА";
+// const userAge = 35;
+// const userLang = "JAVASCRIPT";
+// const testString = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${userName}, МЕНІ ${userAge} РОКІВ, Я ВИВЧАЮ ${userLang}`;
+// console.log(testString);
+
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+// const usernName = prompt("Введіть ваше ім`я").toUpperCase();
+// const userMail = prompt("Введіть вашу пошту");
+// const userTel = prompt("Введіть ваш телефон");
+// const testString = `КОРИСТУВАЧ ${usernName} ВИКОРИСТОВУЄ ${userMail} ПОЧТУ І ${userTel} ТЕЛЕФОН`;
+// console.log(testString);
+
+// ЗАДАЧА 3
+// ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
+
+// const var1 = Number(prompt("Enter price tel"));
+// const var2 = +prompt("Enter price tab");
+// const var3 = +prompt("Enter price desk");
+// const result = var1 + var2 + var3;
+// console.log(result);
