706665cf56574c8033baf9bbc52f9bd129efb2e2
ilchenko82 <s.ilchenko.mb@gmail.com>
2023-03-05 12:29:16 +0200

    third comit

--- js/02_lesson.js
+++ js/02_lesson.js
@@ -51,23 +51,185 @@
 // sum() -повертає суму збережених значень;
 // mult() -перемножує значення збережені і повертає результат;
 
-const calculator = {
-  a: null,
-  b: null,
+// const calculator = {
+//   a: null,
+//   b: null,
 
-  read() {
-    this.a = Number(prompt('введіть значення а'));
-    this.b = Number(prompt('введіть значення b'));
-  },
+//   read() {
+//     this.a = Number(prompt('введіть значення а'));
+//     this.b = Number(prompt('введіть значення b'));
+//   },
 
-  sum() {
-    return this.a + this.b;
-  },
+//   sum() {
+//     return this.a + this.b;
+//   },
 
-  mult() {
-    return this.a * this.b;
-  },
-};
-calculator.read();
-console.log(`sum`, calculator.sum());
-console.log(`mult`, calculator.mult());
+//   mult() {
+//     return this.a * this.b;
+//   },
+// };
+// calculator.read();
+// console.log(`sum`, calculator.sum());
+// console.log(`mult`, calculator.mult());
+
+
+// ЗАДАЧА 1
+// Напишіть дві функції:
+// letMeSeeYourName(callback) - запитує імя користувача , через prompt і викликає колбек функцію (callback)
+// greet(name) - колбек , який приймає імя і логінує в консоль рядок  "Привіт" + імя
+
+
+// function letMeSeeYourName(callback) {
+
+// 	let userName = prompt('Назвіть ваше Імя');
+// 	callback(userName);
+
+// }
+// function greet(name) {
+// 	console.log(`Привіт ${name}`);
+
+// }
+
+// letMeSeeYourName(greet);
+
+
+
+
+// ЗАДАЧА 2
+// Напишіть дві функції:
+// makeProduct(name, price, callback) -приймає імя та ціну товару, а також колбек.Функція створює обєкт товару, добавляє йому унікальний ідентифікатор, в значення id і викликає колбек, передаючи йому створений обєкт.
+// showProduct(product) - колбек приймає обєкт продукта і логінує його в консоль;
+// makeProduct("Холодильник", 10000, logger);
+
+// function makeProduct(name, price, callback) {
+
+// 	let product = {
+// 		name, 
+// 		price, 
+// 		id: Date.now(),
+// 	}
+// 	callback(product);
+// }
+
+// function showProduct(product) {
+
+// 	console.log(product);
+// }
+ 
+// makeProduct("Холодильник", 10000, showProduct);
+// makeProduct("праска", 500, showProduct);
+// makeProduct("сушилка", 2500, showProduct);
+
+
+// ЗАДАЧА 3
+// МАЄМО МАСИВ, ПОТРІБНО:
+// 1)получити масив вчених які народились у 19ст;
+// 2)порахувати загальну суму років скільки прожили вчені , і визначити середнє значення;
+// 3)відсортувати по алфавіту;
+// 4)відсортувати по кількості прожитих років;
+// 5)відфільтрувати, хто народився в 15,16,17ст;
+// 6)знайти рік народження Albert Einstein;
+// 7)визначити, чи усі вчені працювали у 19ст;
+
+
+
+const scientists = [
+    {
+      name: "Albert",
+      surname: "Einstein",
+      born: 1879,
+      dead: 1955,
+      id: 1,
+    },
+    {
+      name: "Isaac",
+      surname: "Newton",
+      born: 1643,
+      dead: 1727,
+      id: 2,
+    },
+    {
+      name: "Galileo",
+      surname: "Galilei",
+      born: 1564,
+      dead: 1642,
+      id: 3,
+    },
+    {
+      name: "Marie",
+      surname: "Curie",
+      born: 1867,
+      dead: 1934,
+      id: 4,
+    },
+    {
+      name: "Johannes",
+      surname: "Kepler",
+      born: 1571,
+      dead: 1630,
+      id: 5,
+    },
+    {
+      name: "Nicolaus",
+      surname: "Copernicus",
+      born: 1473,
+      dead: 1543,
+      id: 6,
+    },
+    {
+      name: "Max",
+      surname: "Planck",
+      born: 1858,
+      dead: 1947,
+      id: 7,
+    },
+    {
+      name: "Katherine",
+      surname: "Blodgett",
+      born: 1898,
+      dead: 1979,
+      id: 8,
+    },
+    {
+      name: "Ada",
+      surname: "Lovelace",
+      born: 1815,
+      dead: 1852,
+      id: 9,
+    },
+  
+    {
+      name: "Lise",
+      surname: "Meitner",
+      born: 1878,
+      dead: 1968,
+      id: 11,
+    },
+    {
+      name: "Sarah E.",
+      surname: "Goode",
+      born: 1855,
+      dead: 1905,
+      id: 10,
+    },
+    {
+      name: "Hanna",
+      surname: "Hammarström",
+      born: 1829,
+      dead: 1909,
+      id: 12,
+    },
+  ];
+
+//   // 1) получити масив вчених які народились у 19ст;
+
+//   const born = scientists.filter(({born}) => born >= 1800 && born < 1900).map(({name, surname}) =>`${name} ${surname}`)
+//   console.log(born);
+
+
+// 2)порахувати загальну суму років скільки прожили вчені , і визначити середнє значення;
+
+const born = scientists.reduce((acc, {born, dead}) => acc + (dead-born), 0);
+
+console.log(born/scientists.length);
+
