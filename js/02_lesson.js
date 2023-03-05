6b5d266384f3b441667c89780cc3df1dd1b8dd12
Voinash Denys <desdenisandqwerty@gmail.com>
2023-03-05 11:09:19 +0200

    task 2

--- js/02_lesson.js
+++ js/02_lesson.js
@@ -1,4 +1,3 @@
-
 // ЗАДАЧА 1
 // Напишіть скрипт,для обєкта user в послідовності:
 
@@ -7,22 +6,41 @@
 // замінює значення  premium на false
 // виводить отримане обєкта user в форматі ключ:значення використовуючи  Object.keys() и for...of
 
- const user = {
-   name: "Mango",
-   age: 20,
-   hobby: "html",
-   premium: true,
-};
+//  const user = {
+//    name: "Mango",
+//    age: 20,
+//    hobby: "html",
+//    premium: true,
+// };
 
-user.mood = "happy";
-user.hobby = "skydiving";
-user.premium = false;
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
 
+// const ourKeys = Object.keys(user);
+// // console.log(ourKeys(ourKeys));
+// for(let key of ourKeys) {
 
-const ourKeys = Object.keys(user);
-// console.log(ourKeys(ourKeys));
-for(let key of ourKeys) {
-	
-	console.log(`${key}: ${user[key]}`);
+// 	console.log(`${key}: ${user[key]}`);
 
-}
+// }
+// ЗАДАЧА 2
+// У вас є обєкт , у якому зберігаються зарплати нашої команди.
+// Напишіть код , для підрахунку всіх зарплат і збережіть результат в змінні sum
+// Повинно получитись 390, якщо обєкт salaries пустий , то результат буде 0
+// ЩОБ МОЖНА БУЛО ПЕРЕВИКОРИСТОВУВАТИ ЛОГІКУ, ТОМУ ПИШЕМО ФУНКЦІЮ
+
+// const salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+// function total(salaries) {
+//   const values = Object.values(salaries);
+//   let sum = 0;
+//   for (let value of values) {
+//     sum += value;
+//   }
+//   return sum;
+// }
+// console.log(total(salaries));
